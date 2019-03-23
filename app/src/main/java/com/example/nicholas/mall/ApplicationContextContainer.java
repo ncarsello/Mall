45c8a0b4d3dc1e6e622ce8a0800a200bf305e73b
nicholas <ncarsello@luc.edu>
2019-03-22 20:48:58 -0500

    Add ApplicationContext Container

---
+++ app/src/main/java/com/example/nicholas/mall/ApplicationContextContainer.java
@@ -0,0 +1,8 @@
+package com.example.nicholas.mall;
+
+from springpython.context import ApplicationContext container = ApplicationContext(WebServiceContainer()) container.get_object(“my_web_server”)
+
+
+
+public class ApplicationContextContainer {
+}
