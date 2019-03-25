fd9130977b539f770432b0dd77729c0429ca38b9
nicholas <ncarsello@luc.edu>
2019-03-25 09:33:00 -0500

    Update facility

--- app/src/main/java/com/example/nicholas/mall/Facility.java
+++ app/src/main/java/com/example/nicholas/mall/Facility.java
@@ -1,4 +1,5 @@
 package com.example.nicholas.mall;
+package Facility;
 
 /**
  * Created by nicholas on 12 March  2019.
@@ -23,13 +24,36 @@ public interface Facility {
 
     public object listStores();
 
-    public object getStoreInformation (return storeInformation);
+    public object getStoreInformation(return storeInformation);
 
-    public object setStoreInformation (this.storeInformation=storeinformation);
+    public object setStoreInformation(this.storeInformation=storeinformation);
 
     public object addNewStore();
 
     public object addStoreDetail();
 
     public object removeStore();
+
+
+
+    public void setFacilityType(String type);
+
+    public String getFacilityType();
+
+    public void setFacilityName(String name);
+
+    public String getFacilityName();
+
+    public void setEmployeeName(String name);
+
+    public String getEmployeeName();
+
+    public void setEmployeePosition(String position);
+
+    public String getEmployeePosition();
+
+    public void setIsOpen(boolean b);
+
+    public boolean getIsOpen();
+
 }
