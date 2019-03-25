a1c810049e98cb6307bff568738c35362e8464c0
nicholas <ncarsello@luc.edu>
2019-03-25 09:31:40 -0500

    update main.java

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -1,13 +1,45 @@
 import java.util.ArrayList;
 import java.util.List;
-
 import com.example.Mall.*;
 import com.example.Employees.*;
 import com.example.Stores.*;
+import Facility.ClothingStore;
+import Facility.Mall;
+import Facility.Stores;
+import FacilityUse.FacilityUse;
+import FacilityMaintenance.Maintenance;
+import Stores.Store;
+import com.example.nicholas.mall.Mall;
 
+public class main {
+    public static void main(String []args){
+        Mall mall = new Mall();
 
-public class Main {
-    public static void main(String [] args){
+        mall.setFacilityName("Old Orchard");
+        mall.setFacilityType("Mall");
+        mall.setEmployeeName("Bill");
+        mall.setEmployeePosition("Janitor");
+
+        ClothingStore ross = new ClothingStore();
+        ross.setEmployeeName("Bobby");
+        ross.setEmployeePosition("Cashier");
+        ross.setFacilityName("Ross");
+        ross.setIsOpen(true);
+        ross.setRequireMaintenance(true);
+
+        System.out.println("++++++++++" + mall.getFacilityName() + " " + mall.getFacilityType() + "++++++++++");
+        mall.addStore(ross);
+        mall.getStoreName();
+        mall.getStoreNumber();
+        System.out.println(mall.getStoreName());
+        System.out.println(mall.getStoreNumber().getIsOpen());
+        mall.requireMaintenance(ross);
+//		System.out.println(mall.getStore().getIsOpen());
+//		mall.requireMaintenance(ross);
+//		System.out.println(mall.getStoreName());
+//		mall.getStoreList().get(0).setFacilityName("Ross");
+//		System.out.println(mall.getStoreList().get(0).getFacilityName());
+
         Mall mall = new Mall();
         ArrayList<Store> mallStores = new ArrayList<Store>();
 
