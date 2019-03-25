bfee586233950925204d50cdd7e346caf0af3e0b
nicholas <ncarsello@luc.edu>
2019-03-24 19:41:37 -0500

    Add CommandFactory (involving beanfactory)

--- app/src/main/java/com/example/nicholas/mall/CommandManager.java
+++ app/src/main/java/com/example/nicholas/mall/CommandManager.java
@@ -10,14 +10,11 @@ public class CommandManager implements BeanFactoryAware {
     private BeanFactory beanFactory;
 
     public Object process(Map commandState) {
-        // grab a new instance of the appropriate Command
         Command command = createCommand();
-        // set the state on the (hopefully brand new) Command instance
         command.setState(commandState);
         return command.execute();
     }
 
-    // the Command returned here could be an implementation that executes asynchronously, or whatever
     protected Command createCommand() {
         return (Command) this.beanFactory.getBean("command"); // notice the Spring API dependency
     }
