3b5b18b9358649d2af533e69a76f1a203d01124e
nicholas <ncarsello@luc.edu>
2019-03-25 09:28:44 -0500

    Add clothingstore.java

---
+++ app/src/main/Stores/clothingstore.java
@@ -0,0 +1,23 @@
+package Facility;
+
+import java.util.*;
+
+public class ClothingStore extends Stores{
+    private boolean requireMaintenance;
+//	private List<String> merchandise = new ArrayList<String>();
+
+    public void setRequireMaintenance(boolean yes){
+        this.requireMaintenance = yes;
+    }
+    public boolean getRequireMaintenance(){
+        return requireMaintenance;
+    }
+
+//	public void addMerchandise(String name){
+//		merchandise.add(name);
+//	}
+//
+//	public List<String> getMerchandise(){
+//		return merchandise;
+//	}
+}
