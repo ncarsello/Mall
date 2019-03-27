b50972be24b361c50dcab688bca44019f5944d44
nicholas <ncarsello@luc.edu>
2019-03-27 11:47:57 -0500

    Update Department.java

--- app/src/main/Stores/Department.java
+++ app/src/main/Stores/Department.java
@@ -39,3 +39,23 @@ public class Department extends Store {
         this.role = cashierrole;
     }role;
     }
+
+    package Facility;
+
+    private String name;
+    private String type;
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public void setType(String type){
+        this.type = type;
+    }
+    public String getType(){
+        return type;
+    }
+}
